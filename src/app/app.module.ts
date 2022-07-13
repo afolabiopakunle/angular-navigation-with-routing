01dd4605a4dcc8af939890b3a78f757b94c8d187
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-13 22:38:21 +0100

    navbar and first route created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { Route, RouterModule } from '@angular/router';
 
+const routes: Route[] = [
+  {path: '', component: HomeComponent}
+]
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent, PostsComponent, NavbarComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
