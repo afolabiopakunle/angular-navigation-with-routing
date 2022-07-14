61bd7435998c6d590b02e70a77ec742c1cb319e6
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-14 08:22:37 +0100

    more components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { PostsComponent } from './posts/posts.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { Route, RouterModule } from '@angular/router';
+import { FollowersComponent } from './followers/followers.component';
 
 const routes: Route[] = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'followers', component: FollowersComponent}
 ]
 @NgModule({
   imports:      [ BrowserModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, PostsComponent, NavbarComponent, HomeComponent ],
+  declarations: [ AppComponent, PostsComponent, NavbarComponent, HomeComponent, FollowersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
