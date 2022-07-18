f15ec8771e8a73acc5893dc42eb33ddacfcc77fc
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-18 08:13:36 +0100

    profile component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { Route, RouterModule } from '@angular/router';
 import { FollowersComponent } from './followers/followers.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Route[] = [
   {path: '', component: HomeComponent},
   {path: 'posts', component: PostsComponent},
+  {path: 'profile/:username', component: ProfileComponent},
   {path: 'followers', component: FollowersComponent}
 ]
 @NgModule({
   imports:      [ BrowserModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, PostsComponent, NavbarComponent, HomeComponent, FollowersComponent ],
+  declarations: [ AppComponent, PostsComponent, NavbarComponent, HomeComponent, FollowersComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
