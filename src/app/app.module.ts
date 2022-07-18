0beeb14336561ed98e6d0ed7b642bd2084eac3bf
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-18 11:54:48 +0100

    HttpModule imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FollowersComponent } from './followers/followers.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { GithubFollowersService } from './shared/followers.service';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Route[] = [
   {path: '', redirectTo: 'followers', pathMatch: 'full'},
@@ -20,7 +21,7 @@ const routes: Route[] = [
   {path: '**', component: NotFoundComponent }
 ]
 @NgModule({
-  imports:      [ BrowserModule, RouterModule.forRoot(routes) ],
+  imports:      [ BrowserModule, RouterModule.forRoot(routes), HttpClientModule ],
   declarations: [ AppComponent, PostsComponent, NavbarComponent, HomeComponent, FollowersComponent, ProfileComponent, NotFoundComponent ],
   bootstrap:    [ AppComponent ],
   providers: [GithubFollowersService]
